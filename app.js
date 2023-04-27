e87ac3ec34d82136f96c3aaa1e366576f99c8acb
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-04-27 10:30:25 +0400

    feat: add lock to counter

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: '',
-      lastName: '',
+      name: "",
+      lastName: "",
       // fullname: ''
     };
   },
-  watch:{
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
     // name(value){
     //   if(value === ''){
     //     return '';
@@ -23,18 +31,17 @@ const app = Vue.createApp({
     //   }
     // }
   },
-  computed:{
-    fullname(){
-       if(this.name === '' || this.lastName === ''){
-        return '';
+  computed: {
+    fullname() {
+      if (this.name === "" || this.lastName === "") {
+        return "";
       }
-      return this.name + ' ' + this.lastName
-    }
+      return this.name + " " + this.lastName;
+    },
   },
   methods: {
-   
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value + " " + lastName;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -43,11 +50,11 @@ const app = Vue.createApp({
       this.counter = this.counter - num;
       // this.counter--;
     },
-    resetInput(){
-      this.name='';
-      this.lastName='';
-    }
-  }
+    resetInput() {
+      this.name = "";
+      this.lastName = "";
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
