7aefcba5566a234e300a6f516a7621dde900ba40
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-04-27 09:35:55 +0400

    feat: add name-surname genertor

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods:{
+    setName(event,LastName){
+      this.name = event.target.value + " " + LastName;     
+    },
     add(num){
       this.counter = this.counter + num;
     },
