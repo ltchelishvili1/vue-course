53788fc86f8430274f32da6ba1bb3ef3863fad49
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-04-29 23:12:19 +0400

    feat: add vue client user contact

---
+++ vue-first-project/src/components/FriendContact.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }}</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        name: "levani",
+        phone: "557700447",
+        email: "test@test.com",
+      },
+    };
+  },
+  methods:{
+    toggleDetails(){
+        this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
