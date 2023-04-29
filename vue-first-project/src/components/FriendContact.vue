aa6d03995e2162ea87698bf369af6c38e0434c55
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-04-30 00:19:23 +0400

    feat: add emit validation example

--- vue-first-project/src/components/FriendContact.vue
+++ vue-first-project/src/components/FriendContact.vue
@@ -41,6 +41,17 @@ export default {
       // },
     },
   },
+  emits: ['toggle-favorite'],
+  // emits: {
+  //   'toggle-favorite': function(id){
+  //     if(id){
+  //       return true;
+  //     }else{
+  //       console.warn('id is missing')
+  //       return false;
+  //     }
+  //   }
+  // },
   data() {
     return {
       detailsAreVisible: false,
