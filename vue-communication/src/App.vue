4a4ae42c7504d2c457641cc08a418310588d5204
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-04-30 01:12:42 +0400

    feat: add provide-inject

--- vue-communication/src/App.vue
+++ vue-communication/src/App.vue
@@ -1,10 +1,12 @@
+
+
 <template>
   <div>
     <active-element
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base @select-topic="activateTopic"></knowledge-base>
   </div>
 </template>
 
@@ -13,22 +15,22 @@ export default {
   data() {
     return {
       topics: [
-        {
-          id: 'basics',
-          title: 'The Basics',
-          description: 'Core Vue basics you have to know',
-          fullText:
-            'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
-        },
-        {
-          id: 'components',
-          title: 'Components',
-          description:
-            'Components are a core concept for building Vue UIs and apps',
-          fullText:
-            'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
-        },
-      ],
+      {
+        id: 'basics',
+        title: 'The Basics',
+        description: 'Core Vue basics you have to know',
+        fullText:
+          'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
+      },
+      {
+        id: 'components',
+        title: 'Components',
+        description:
+          'Components are a core concept for building Vue UIs and apps',
+        fullText:
+          'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
+      },
+    ],
       activeTopic: null,
     };
   },
@@ -36,6 +38,24 @@ export default {
     activateTopic(topicId) {
       this.activeTopic = this.topics.find((topic) => topic.id === topicId);
     },
+  },
+
+  mounted(){
+    setTimeout(() => {
+      this.topics.push({
+        id:'events',
+        title: 'Events',
+        description: 'Events are important in Vue',
+        fullText: 'Events allow you lot'
+
+      })
+    },3000)
+  },
+
+  provide() {
+    return {
+        topics: this.topics
+    }
   },
 };
 </script>
