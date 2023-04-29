af24298840836620f7cd4401a6e0e08e1845c469
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-04-30 01:17:42 +0400

    refactor: provide inject function

--- vue-communication/src/App.vue
+++ vue-communication/src/App.vue
@@ -6,7 +6,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base ></knowledge-base>
   </div>
 </template>
 
@@ -54,7 +54,8 @@ export default {
 
   provide() {
     return {
-        topics: this.topics
+        topics: this.topics,
+        selectTopic: this.activateTopic
     }
   },
 };
