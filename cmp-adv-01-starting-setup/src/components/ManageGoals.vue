08bde3152da51fe92702524bd68f06a8419840cc
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-05-03 16:06:31 +0400

    components

---
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <Teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is ivalid!</h2>
+        <p>enter few charachters</p>
+        <button @click="confirmError">Okay</button>
+      </error-alert>
+    </Teleport>
+  </div>
+</template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
