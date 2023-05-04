bceca0124bc0a579a8f511a532c5bcdf2fb32dee
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-05-04 18:51:32 +0400

    feat: vuex

---
+++ vuex-01-starting-setup/src/components/FavoriteValue.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h3>{{ counter }}</h3>
+    <p>we do more...</p>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    counter() {
+        return this.$store.getters['numbers/normalizedCounter'];
+    },
+  },
+};
+</script>
---
+++ vuex-01-starting-setup/src/components/UserAuth.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <button @click="login" v-if="!isAuth">Login</button>
+    <button @click="logout" v-else>Logout</button>
+  </section>
+</template>
+
+<script>
+export default{
+  methods:{
+    login(){
+   
+      this.$store.dispatch('login');
+    },
+    logout(){
+      this.$store.dispatch('logout');
+    }
+  },
+  computed:{
+    isAuth(){
+      return this.$store.getters.userIsAuthenticated
+    }
+  }
+}
+</script>
