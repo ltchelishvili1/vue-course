633290aa6bab29fac5bf755ea7048e6770dbfe66
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-05-04 23:12:25 +0400

    feat: vuex landing

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -2,13 +2,53 @@
   <section>
     <section>FILTER</section>
     <section>
+      <base-card>
         <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
+          <base-button mode="outline">Refresh</base-button>
+          <base-button link to="/register">Register as Coach</base-button>
         </div>
-      <ul>
-        LIST OF COACHES
-      </ul>
+        <ul v-if="hasCoaches">
+          <coach-item
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :id="coach.id"
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+          ></coach-item>
+        </ul>
+        <h3 v-else>No coaches found.</h3>
+      </base-card>
     </section>
   </section>
 </template>
+
+<script>
+
+import CoachItem from '../../components/coaches/CoachItem.vue';
+export default {
+  components: { CoachItem },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
