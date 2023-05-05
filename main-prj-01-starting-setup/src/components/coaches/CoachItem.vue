aa6762dbfd5047f97dc08d534341da55a5c9e636
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-05-05 11:47:38 +0400

    feat: add send request

--- main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
+++ main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
@@ -20,7 +20,7 @@ export default {
       return this.firstName + ' ' + this.lastName;
     },
     coachContactLink() {
-      return this.$route.path + '/' + this.id + '/contacts';
+      return this.$route.path + '/' + this.id + '/contact';
     },
     coachDetailsLink() {
       return this.$route.path + '/' + this.id;
