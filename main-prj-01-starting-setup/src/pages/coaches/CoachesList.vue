a0f94415a68c05cf1428fe9af5ee4fe4301377db
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-05-05 12:23:27 +0400

    feat: store and display data to firebase

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -4,7 +4,7 @@
     <section>
       <base-card>
         <div class="controls">
-          <base-button mode="outline">Refresh</base-button>
+          <base-button mode="outline" :click="loadCoaches">Refresh</base-button>
           <base-button v-if="!isCoach" link to="/register">Register as Coach</base-button>
         </div>
         <ul v-if="hasCoaches">
@@ -61,10 +61,16 @@ export default {
       return this.$store.getters['coaches/hasCoaches'];
     },
   },
+  created(){
+    this.loadCoaches()
+  },
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
+    loadCoaches(){
+      this.$store.dispatch('coaches/loadCoaches')
+    }
   },
 };
 </script>
