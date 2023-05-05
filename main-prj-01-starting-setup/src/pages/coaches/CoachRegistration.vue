107f983199dd1aaf71ed1bdba07a84ffc8effcf7
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-05-05 10:41:25 +0400

    feat: add registration form

--- main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,17 @@
 <template>
-    <p>regis</p>
+  <section>
+    <base-card>
+      <h2>Register</h2>
+      <coach-form></coach-form
+    ></base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
