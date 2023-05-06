a3d9343b61c8bebece8163bd3251cd5055c2a8c6
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-05-06 15:52:20 +0400

    feat: ref example

--- composition-01-starting-setup/src/App.vue
+++ composition-01-starting-setup/src/App.vue
@@ -5,7 +5,8 @@
     <button @click="setAge">Change Age</button>
     <div>
       <input type="text" placeholder="First Name" v-model="firstName" />
-      <input type="text" placeholder="Last Name" v-model="lastName" />
+      <input type="text" placeholder="Last Name" ref="lastNameInput" />
+      <button @click="setLastName">Set Last Name</button>
     </div>
   </section>
 </template>
@@ -18,6 +19,7 @@ export default {
     /*  const uName = ref('maximilian');*/
     const uAge = ref(31);
     const firstName = ref('');
+    const lastNameInput = ref(null)
     const lastName = ref('');
     // const user = reactive({
     //   name: 'maximilia',
@@ -36,12 +38,17 @@ export default {
       uAge.value = 32;
     }
 
+    function setLastName(){
+      lastName.value = lastNameInput.value.value
+    }
+
     return {
       userName: uName,
       age: uAge,
       setAge: setNewAge,
       firstName,
-      lastName,
+      lastNameInput,
+      setLastName
     };
   },
 };
